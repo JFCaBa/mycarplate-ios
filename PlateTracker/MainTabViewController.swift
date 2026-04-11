ca79f4045df3d209095b977334c6b4311afff71b
Jose <jfca68@gmail.com>
2026-04-11 20:29:46 +0200

    fix(ios): ensure configure() is called before view hierarchy loads

--- PlateTracker/MainTabViewController.swift
+++ PlateTracker/MainTabViewController.swift
@@ -2,43 +2,41 @@
 //  MainTabViewController.swift
 //  PlateTracker
 //
-//  Created by Jose on 21/07/2025.
-//
 
 import UIKit
 
 final class MainTabBarController: UITabBarController {
 
     private let scanViewModel = ScanViewModel()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabs()
         configureTabBarAppearance()
     }
-    
+
     private func configureTabBarAppearance() {
         let appearance = UITabBarAppearance()
-        appearance.configureWithOpaqueBackground() // removes transparency
-        appearance.backgroundColor = .systemBackground 
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemBackground
 
         tabBar.standardAppearance = appearance
-        tabBar.scrollEdgeAppearance = appearance // Important for iOS 15+
+        tabBar.scrollEdgeAppearance = appearance
     }
-    
+
     private func setupTabs() {
         let scanVC = ScanViewController()
-        scanVC.tabBarItem = UITabBarItem(title: "Scan", image: UIImage(systemName: "camera"), tag: 0)
         scanVC.configure(with: scanViewModel)
-        
+        scanVC.tabBarItem = UITabBarItem(title: "Scan", image: UIImage(systemName: "camera"), tag: 0)
+
         let resultsVC = ResultsViewController()
-        resultsVC.tabBarItem = UITabBarItem(title: "Results", image: UIImage(systemName: "list.bullet"), tag: 1)
         resultsVC.configure(with: scanViewModel)
-        
+        resultsVC.tabBarItem = UITabBarItem(title: "Results", image: UIImage(systemName: "list.bullet"), tag: 1)
+
         let mapVC = MapViewController()
-        mapVC.tabBarItem = UITabBarItem(title: "Map", image: UIImage(systemName: "map"), tag: 2)
         mapVC.configure(with: scanViewModel)
-        
+        mapVC.tabBarItem = UITabBarItem(title: "Map", image: UIImage(systemName: "map"), tag: 2)
+
         viewControllers = [
             UINavigationController(rootViewController: scanVC),
             UINavigationController(rootViewController: resultsVC),
